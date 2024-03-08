9c750021410edacc68b9f7ad72268ba8489c337e
Segun Aruleba <segsywealthprosperity@gmail.com>
2024-03-08 14:03:12 +0100

    add mv slider plugin to repository

---
+++ vendor/flexslider/flexslider.js
@@ -0,0 +1,10 @@
+
+jQuery(window).load(function () {
+    JQuery('.flexslider').flexslider({
+        animation: "slide",
+        touch: true,
+        directionNav: false,
+        smoothHeight: true,
+        controlNav: true,
+    });
+});
